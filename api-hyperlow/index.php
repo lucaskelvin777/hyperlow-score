50b5e140fe2fef0dcb818f77ed90f001b2db2be9
Lucas Kelvin <lucas@genya.com.br>
2020-06-08 22:37:06 -0300

    fix php sql

--- api-hyperlow/index.php
+++ api-hyperlow/index.php
@@ -7,20 +7,20 @@ require 'conexao.php';
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 $pagination = new ZebraPagination();
-$records_per_page = 10;
+$records_per_page = 12;
 $pagination->records_per_page($records_per_page);
 
 function records($pesquisa = "")
 {
   $db = connection();
   if (empty($pesquisa)) {
-    $sql = "SELET count(*) as rows FROM pontuacao";
+    $sql = "SELECT count(*) as rows FROM pontuacao";
     
     $stmt = $db->prepare($sql);
     $stmt->execute();
     return $stmt->fetch()['rows'];
   } else {
-    $sql = "SELET count(*) as rows FROM pontuacao WHERE nome like '%$pesquisa%'";
+    $sql = "SELECT count(*) as rows FROM pontuacao WHERE nome like '%$pesquisa%'";
     $stmt = $db->prepare($sql);
     $stmt->execute();
     return $stmt->fetch()['rows'];
@@ -36,33 +36,28 @@ function get()
   $pdo  = connection();
   $stmt = $pdo->prepare($sql);
   $stmt->execute();
-  echo json_encode($stmt->fetchAll());
+  return $stmt->fetchAll();
 }
 function getWithSearch($pesquisa)
 {
   global $pagination;
   global $records_per_page;
-  $pagination->records(records());
-  $sql = "SELECT id, nome,clicks_r, reinicio_fase, batidas, date_format(created_at, '%d/%m/%Y') as created_at FROM `pontuacao` WHERE nome like '%$pesquisa%' order by id desc LIMIT " . (($pagination->get_page() - 1) * $records_per_page) . ', ' . $records_per_page . '';
+  $pagination->records(records($pesquisa));
+  $sql = "SELECT id, nome,clicks_r, reinicio_fase, batidas, date_format(created_at, '%d/%m/%Y') as created_at FROM `pontuacao` WHERE nome like '%$pesquisa%' order by clicks_r,reinicio_fase,batidas  LIMIT " . (($pagination->get_page() - 1) * $records_per_page) . ', ' . $records_per_page . '';
   $pdo  = connection();
   $stmt = $pdo->prepare($sql);
   $stmt->execute();
-  echo json_encode($stmt->fetchAll());
+  return $stmt->fetchAll();
 }
 
 
 try {
-
-  
   if (isset($_GET['search']) == true) {
     $search = filter_var($_GET['search'], FILTER_SANITIZE_STRING);
-    getWithSearch($search);
+    echo json_encode(['data'=>getWithSearch($search), paginas=>$pagination->get_pages()]);
   } else {
-    get();
-  }
-  if (isset($_GET['paginas'])) {
-    echo json_encode(['paginas'=>$pagination->get_pages()]);
-    return;
+    
+   echo json_encode(['data'=>get(), paginas=>$pagination->get_pages()]);
   }
 } catch (Exception $ex) {
   return false;
